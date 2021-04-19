601313967a4db3594ca264dc25e7c10bf1b01d4e
hanan-tuwaiq <hanan.hakami@tuwaiq.edu.sa>
2021-04-20 00:16:03 +0300

    second commit

--- WK2D2_Tokenizer/Program.cs
+++ WK2D2_Tokenizer/Program.cs
@@ -1,11 +1,12 @@
 ﻿using System;
+using System.Collections;
 
 namespace WK2D2_Tokenizer
 {
     class Program
     {
 
-        public class Token 
+        public class Token
         {
             public string type; // id | number
             public string value;
@@ -32,11 +33,11 @@ namespace WK2D2_Tokenizer
                 this.lineNumber = 1;
             }
 
-            public char peek()
+            public char peek(int p = 1)
             {
                 if (this.hasMore())
                 {
-                    return this.input[this.currentPosition + 1];
+                    return this.input[this.currentPosition + p];
                 }
                 else
                 {
@@ -55,9 +56,9 @@ namespace WK2D2_Tokenizer
                 return currentChar;
             }
 
-            public bool hasMore()
+            public bool hasMore(int p = 1)
             {
-                return (this.currentPosition + 1) < this.input.Length;
+                return (this.currentPosition + p) < this.input.Length;
             }
 
             public Token tokenize(Tokenizable[] handlers)
@@ -95,7 +96,23 @@ namespace WK2D2_Tokenizer
                 {
                     token.value += t.next();
                 }
-
+                if (t.peek() == '.')
+                {
+                    token.value += t.next();
+                    while (t.hasMore() && Char.IsDigit(t.peek()))
+                    {
+                        token.value += t.next();
+                    }
+                    String[] str = token.value.Split(".");
+                    if (str[1].Length < 8)
+                    {
+                        token.type = "Float";
+                    }
+                    else
+                    {
+                        token.type = "Double";
+                    }
+                }
                 return token;
             }
         }
@@ -104,7 +121,7 @@ namespace WK2D2_Tokenizer
         {
             public override bool tokenizable(Tokenizer t)
             {
-                return t.hasMore() && Char.IsWhiteSpace(t.peek());
+                return t.hasMore() && Char.IsWhiteSpace(t.peek()) && t.peek() != '\n';
             }
 
             public override Token tokenize(Tokenizer t)
@@ -119,13 +136,27 @@ namespace WK2D2_Tokenizer
                 {
                     token.value += t.next();
                 }
-
                 return token;
             }
         }
 
         public class IdTokenizer : Tokenizable
         {
+            public bool isKeyword(string modifer)
+            {
+                string[] keywords = { "public", "private", "protected", "internal","true", "false"
+                ,"abstract" ,"async", "const", "event", "extern", "new", "override", "partial"};//list not complete
+                int flag = 0;
+                foreach (var key in keywords)
+                {
+                    if (modifer == key)
+                    {
+                        flag = 1;
+                    }
+
+                }
+                return flag == 1;
+            }
             public override bool tokenizable(Tokenizer t)
             {
                 return t.hasMore() && (Char.IsLetter(t.peek()) || t.peek() == '_');
@@ -143,28 +174,195 @@ namespace WK2D2_Tokenizer
                 {
                     token.value += t.next();
                 }
-
+                if (isKeyword(token.value))
+                {
+                    token.type = "keyword";
+                }
                 return token;
             }
         }
-
-        static void Main(string[] args)
+        //one line comment
+        public class OneLineCommentTokenizer : Tokenizable
         {
-            /// After refactoring test
-            string testCase = "78  a _PORT _ j u8 8u 500";//u8?
-            Tokenizer t = new Tokenizer(testCase);
-            Tokenizable[] handlers = new Tokenizable[] {
-                  new NumberTokenizer()
-                , new WhiteSpaceTokenizer()
-                , new IdTokenizer()
-            };
-            Token token = t.tokenize(handlers);
-            while (token != null)
+            public override bool tokenizable(Tokenizer t)
             {
-                Console.WriteLine("Token Value= " + token.value + " type= " + token.type);
-                token = t.tokenize(handlers);
+                if (t.hasMore() && t.peek() == '/')
+                {
+                    //Console.WriteLine("first slash");
+                    if (t.hasMore() && t.peek(2) == '/')
+                    {
+                        //Console.WriteLine("second slash");
+                        return true;
+                    }
+                }
+                return false;
             }
+            public override Token tokenize(Tokenizer t)
+            {
+                Token token = new Token();
+                token.type = "one line comment";
+                token.value = "";
+                token.position = t.currentPosition;
+                token.lineNumber = t.lineNumber;
+                while (t.hasMore() && t.peek() != '\n')
+                {
+                    token.value += t.next();
+                }
+                return token;
+            }
+        }
+        //multi line comment
+        public class MultiLineCommentTokenizer : Tokenizable
+        {
+            public override bool tokenizable(Tokenizer t)
+            {
+                if (t.hasMore() && t.peek() == '/')
+                {
+                    if (t.hasMore(2) && t.peek(2) == '*')
+                    {
+                        return true;
+                    }
+                }
+                return false;
+            }
+            public override Token tokenize(Tokenizer t)
+            {
+                Token token = new Token();
+                token.type = "Mutli Line Comment";
+                token.value = "";
+                token.position = t.currentPosition;
+                token.lineNumber = t.lineNumber;
+                while (t.hasMore())
+                {
+                    if (t.hasMore() && t.peek() == '*')
+                    {
+                        if (t.hasMore() && t.peek(2) == '/')
+                        {
+                            //t.next();
+                            token.value += t.next();
+                            token.value += t.next();
+                            break;
+                        }
+                        token.value += t.next();
+                    }
+                    token.value += t.next();
+                }
+                return token;
+            }
+        }
+        //operators
+        public class OperatorTokenizer : Tokenizable
+        {
+            public bool isOperator(char x)
+            {
+                if (x == '=' || x == '+' || x == '-' || x == '%' || x == '*' || x == '<' || x == '>')
+                    return true;
+                return false;
+            }
+            public override bool tokenizable(Tokenizer t)
+            {
+                return t.hasMore() && (isOperator(t.peek()));
+            }
+            public override Token tokenize(Tokenizer t)
+            {
+                Token token = new Token();
+                token.type = "Operator";
+                token.value = "";
+                token.position = t.currentPosition;
+                token.lineNumber = t.lineNumber;
+                while (t.hasMore() && (isOperator(t.peek())))
+                {
+                    token.value += t.next();
+                }
+                return token;
+            }
+        }
+        //Punctuation
+        public class PunctuationTokenizer : Tokenizable
+        {
+            public override bool tokenizable(Tokenizer t)
+            {
+                return t.hasMore() && Char.IsPunctuation(t.peek());
+            }
+            public override Token tokenize(Tokenizer t)
+            {
+                Token token = new Token();
+                token.type = "Punctuation";
+                token.value = "";
+                token.position = t.currentPosition;
+                token.lineNumber = t.lineNumber;
+                while (t.hasMore() && Char.IsPunctuation(t.peek()))
+                {
+                    token.value += t.next();
+                }
+                return token;
+            }
+        }
+        //lowercase
+        public class LowerCasesTokenizer : Tokenizable
+        {
+            public override bool tokenizable(Tokenizer t)
+            {
+                return t.hasMore() && Char.IsLetter(t.peek()) && Char.IsLower(t.peek());
+            }
+            public override Token tokenize(Tokenizer t)
+            {
+                Token token = new Token();
+                token.type = "Lowercase";
+                token.value = "";
+                token.position = t.currentPosition;
+                token.lineNumber = t.lineNumber;
+                while (t.hasMore() && Char.IsLetter(t.peek()) && Char.IsLower(t.peek()))
+                {
+                    token.value += t.next();
+                }
+                return token;
+            }
+        }
+        //uppercase
+        public class UpperCasesTokenizer : Tokenizable
+        {
+            public override bool tokenizable(Tokenizer t)
+            {
+                return t.hasMore() && Char.IsLetter(t.peek()) && Char.IsUpper(t.peek());
+            }
+            public override Token tokenize(Tokenizer t)
+            {
+                Token token = new Token();
+                token.type = "Uppercase";
+                token.value = "";
+                token.position = t.currentPosition;
+                token.lineNumber = t.lineNumber;
+                while (t.hasMore() && Char.IsLetter(t.peek()) && Char.IsUpper(t.peek()))
+                {
+                    token.value += t.next();
+                }
+                return token;
+            }
+        }
+        static void Main(string[] args)
+            {
+                Console.WriteLine("===============");
+                string testCase = "78 true _PORT ! . 45.67 78.88888888 4566 /*fff*/ ++ = TEA tea //hhhh  5 ";
+                Tokenizer t = new Tokenizer(testCase);
+                Tokenizable[] handlers = new Tokenizable[] {
+                  new OneLineCommentTokenizer()
+                , new WhiteSpaceTokenizer()
+                , new OperatorTokenizer()
+                , new NumberTokenizer()
+                , new IdTokenizer()
+                , new MultiLineCommentTokenizer()
+                , new PunctuationTokenizer()
+                , new UpperCasesTokenizer()
+                , new LowerCasesTokenizer()
+            };
+                Token token = t.tokenize(handlers);
+                while (token != null)
+                {
+                    Console.WriteLine(token.value + " : " + token.type);
+                    token = t.tokenize(handlers);
+                }
 
+            }
         }
     }
-}
